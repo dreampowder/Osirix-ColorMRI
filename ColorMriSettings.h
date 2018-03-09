17d8df7535f5f0f2a8b5e6617460bf95651ec55f
Serdar Coskun <coskun.serdar@gmail.com>
2018-03-09 18:16:01 +0300

    minor updates

---
+++ ColorMriSettings.h
@@ -0,0 +1,12 @@
+//
+//  ColorMriSettings.h
+//  ColorMRI
+//
+//  Created by Serdar Coskun on 22/12/2017.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ColorMriSettings : NSWindowController
+
+@end
---
+++ ColorMriSettings.m
@@ -0,0 +1,24 @@
+//
+//  ColorMriSettings.m
+//  ColorMRI
+//
+//  Created by Serdar Coskun on 22/12/2017.
+//
+
+#import "ColorMriSettings.h"
+
+@interface ColorMriSettings ()
+
+@property (nonatomic, strong) IBOutlet NSBrowser* browser;
+
+@end
+
+@implementation ColorMriSettings
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
